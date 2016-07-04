1b9dae556102ebeedc14f5a2e4b80c81bfa14833
Gregivy <gpoi@mail.ru>
2016-07-05 01:18:27 +0300

    first init

---
+++ cordova/appBeforeBuild.js
@@ -0,0 +1,16 @@
+module.exports = function(ctx) {
+	if (ctx.opts.platforms.indexOf('android') < 0) {
+        return;
+    }
+    var fs = ctx.requireCordovaModule('fs'),
+        path = ctx.requireCordovaModule('path'),
+        deferral = ctx.requireCordovaModule('q').defer();
+    var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android/');
+    var data = fs.readFileSync(platformRoot+'AndroidManifest.xml', 'utf-8');
+    var beforeManifest = data.split("<manifest")[0];
+    var manifesttag = data.split("<manifest")[1].split(">")[0];
+    var afterManifest = data.split("<manifest")[1].split(">")[1];
+  	var newValue = beforeManifest + "<manifest" + manifesttag + ">" + '<uses-feature android:name="android.hardware.camera" android:required="true" />' + afterManifest;
+  	fs.writeFileSync(platformRoot+'AndroidManifest.xml', newValue, 'utf-8');
+
+}
--- main.js
+++ main.js
@@ -25,13 +25,17 @@ var button = new tabris.Button({
   layoutData: {left: 10, top: 10},
   text: "Button"
 }).on("select", function() {
-	fetch("./test.html").then(function(response) {
+	/*fetch("./test.html?"+Math.random()).then(function(response) {
 		return response.text();
 	}).then(function(text) {
 		var webview = new tabris.WebView({
 		  layoutData: {left: 0, top: [button,0], right: 0, bottom: 0}
 		}).set('html',text).appendTo(page);
-	});
+	});*/
+	/*var webview = new tabris.WebView({
+		  layoutData: {left: 0, top: [button,0], right: 0, bottom: 0}
+		}).set('url',"https://davidwalsh.name/demo/camera.php").appendTo(page);*/
+	cordova.InAppBrowser.open("https://davidwalsh.name/demo/camera.php","_blank","location=no");
 }).appendTo(page);
 
 page.open();
