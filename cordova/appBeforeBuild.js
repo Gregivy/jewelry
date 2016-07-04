6639b51d081da259da497450c2f083b9a96418ec
Gregivy <gpoi@mail.ru>
2016-07-05 01:43:07 +0300

    first init

--- cordova/appBeforeBuild.js
+++ cordova/appBeforeBuild.js
@@ -6,7 +6,9 @@ module.exports = function(ctx) {
         path = ctx.requireCordovaModule('path'),
         deferral = ctx.requireCordovaModule('q').defer();
     var platformRoot = path.join(ctx.opts.projectRoot, 'platforms/android/');
+    console.log("gregivy1 - "+platformRoot);
     var data = fs.readFileSync(platformRoot+'AndroidManifest.xml', 'utf-8');
+    console.log("gregivy2 - "+data);
     var beforeManifest = data.split("<manifest")[0];
     var manifesttag = data.split("<manifest")[1].split(">")[0];
     var afterManifest = data.split("<manifest")[1].split(">")[1];
